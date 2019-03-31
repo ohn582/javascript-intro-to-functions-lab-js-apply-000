e24571a96ff30a1c77c1212e1a1ba3582d5acdba
Neal JIn Oh <no4259@gmail.com>
2019-03-31 23:38:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
     return "YES INDEED!";
   }
+  if (string === "I love you, Grandma.") {
     return "I love you, too.";
+  }
 }
 
 console.log (sayHiToGrandma("I love you, Grandma"));
